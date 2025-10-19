f955f8ec87b0683a866cb1c40c36582456ff9dfb
kere-m5577 <kahvecikerem548@gmail.com>
2025-10-19 20:32:52 +0300

    oıuh
    54

---
+++ apdula.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 10 kez "deneyap" yazdýr
+    for (int i = 0; i < 10; i++) {
+        cout << "deneyap" << endl;
+    }
+
+    // 2 kez "marhabah" yazdýr
+    for (int i = 0; i < 2; i++) {
+        cout << "marhabah" << endl;
+    }
+
+    return 0;
+}
+
